3d79fe36c9262f35f691b1c9028313d4d03c94e3
todor.mitev <todor.mitev+work@pfgbulgaria.com>
2023-11-19 11:07:15 +0200

    Added error handle + new version

--- errors.go
+++ errors.go
@@ -8,3 +8,9 @@ import (
 func NewError(params ...interface{}) error {
 	return errors.New(fmt.Sprintf(params[0].(string), params[1:]...))
 }
+
+func HandleWarning(_ any, err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/siper92/core-utils
 
-go 1.20
+go 1.21
 
 require (
 	github.com/fatih/color v1.15.0
